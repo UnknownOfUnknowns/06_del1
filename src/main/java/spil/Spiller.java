bf452ad249999525a35d3e2888db1a74d6cdc571
Unknown Unknown <hans@bilstrupandersen.dk>
2021-09-28 14:22:34 +0200

    Begyndelse på spil-klassen

--- src/main/java/spil/Spiller.java
+++ src/main/java/spil/Spiller.java
@@ -5,12 +5,14 @@ public class Spiller {
     private int point;
     private Terning t1;
     private Terning t2;
+    private boolean slog_2_ens;
 
     public Spiller(){
         sidste_slag = 0;
         point = 0;
         t1 = new Terning();
         t2 = new Terning();
+        slog_2_ens = false;
     }
 
     public int rul_terning(){
@@ -18,8 +20,8 @@ public class Spiller {
         t2.rul();
         int sum = t1.getVærdi() + t2.getVærdi();
         sidste_slag = sum;
+        slog_2_ens = t1.getVærdi() == t2.getVærdi();
         return sum;
-
     }
     public int getPoint() {
         return point;
@@ -27,6 +29,14 @@ public class Spiller {
 
     public int getSidste_slag() {
         return sidste_slag;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public boolean slog_2_ens() {
+        return slog_2_ens;
     }
 
     public void incrementPoint(int point) {
