302c07981b7373e39452140d2b073ec7b94f97b6
josefbaraki <josef9753@outlook.dk>
2021-09-28 14:41:33 +0200

    Spilklassen er foreløbigt implementeret

--- src/main/java/spil/Spil.java
+++ src/main/java/spil/Spil.java
@@ -17,7 +17,27 @@ public class Spil {
         }
     }
     private void tag_tur(Spiller s){
-
+        int sidste_slag = s.getSidste_slag();
+        int slag_værdi = s.rul_terning();
+        switch (slag_værdi){
+            case 2:
+                s.setPoint(0);
+                break;
+            case 12:
+                if (s.getPoint() == 40){
+                    vinder_fundet = true;
+                }else if(sidste_slag == 12){
+                    vinder_fundet = true;
+                }else{
+                    s.incrementPoint(slag_værdi);
+                }
+                break;
+            default:
+                s.incrementPoint(slag_værdi);
+        }
+        if(s.slog_2_ens()) {
+            tag_tur(s);
+        }
     }
 
 }
