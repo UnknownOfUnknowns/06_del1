814389e6ccc8fe869ce368db08bfa24da84d4aaf
Hurtigpilskade <albertmatthaihalkjaer@gmail.com>
2021-09-30 10:05:52 +0200

    Clean up og refacturing

--- src/main/java/spil/Spil.java
+++ src/main/java/spil/Spil.java
@@ -3,41 +3,45 @@ package spil;
 public class Spil {
     private Spiller s1;
     private Spiller s2;
-    private boolean vinder_fundet;
-    public Spil(){
-        s1 = new Spiller();
-        s2 = new Spiller();
-        vinder_fundet = false;
+    private Spiller vinder;
+    private Spiller tur_spiller; //Spiller der har sin tur
+
+    public Spil(Spiller spiller1, Spiller spiller2){
+        s1 = spiller1;
+        s2 = spiller2;
+        vinder = null;
+        tur_spiller = s1;
     }
 
-    public void start_spil(){
-        while(!vinder_fundet){
-            tag_tur(s1);
-            tag_tur(s2);
-        }
+    public Spiller vinder(){
+        return vinder;
     }
-    private void tag_tur(Spiller s){
+
+    public void tag_tur(Spiller s) throws Exception {
+        if (!(s == tur_spiller)) {
+            throw new Exception("Spiller udenfor tur");
+        }
         int sidste_slag = s.getSidste_slag();
         int slag_værdi = s.rul_terning();
-        switch (slag_værdi){
+        switch (slag_værdi) {
             case 2:
                 s.setPoint(0);
                 break;
             case 12:
-                if (s.getPoint() == 40){
-                    vinder_fundet = true;
-                }else if(sidste_slag == 12){
-                    vinder_fundet = true;
-                }else{
+                if (s.getPoint() == 40) {
+                    vinder = s;
+                } else if (sidste_slag == 12) {
+                    vinder = s;
+                } else {
                     s.incrementPoint(slag_værdi);
                 }
                 break;
             default:
                 s.incrementPoint(slag_værdi);
         }
-        if(s.slog_2_ens()) {
-            tag_tur(s);
+        if (!s.slog_2_ens()) {  //skift tur til anden spiller hvis aktuelle spiller ikke har slået to ens
+            tur_spiller = (s == s1) ? s2 : s1;
+
         }
     }
-
 }
--- src/main/java/spil/Spiller.java
+++ src/main/java/spil/Spiller.java
@@ -30,6 +30,14 @@ public class Spiller {
     public int getSidste_slag() {
         return sidste_slag;
     }
+    private int[] getTerningØjne(){
+        return new int[]{t1.getVærdi(), t2.getVærdi()};
+    }
+
+    public int[] getStatus(){
+        int[] øjne = getTerningØjne();
+        return new int[]{øjne[0], øjne[1], getPoint()};
+    }
 
     public void setPoint(int point) {
         this.point = point;
@@ -39,7 +47,8 @@ public class Spiller {
         return slog_2_ens;
     }
 
-    public void incrementPoint(int point) {
-        this.point += point;
+    public void incrementPoint(int p) {
+        this.point += p;
+        if(point>40){point=40;}
     }
 }
